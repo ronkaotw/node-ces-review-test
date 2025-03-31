a9bb547ba9604ba3650deee52fa3615f599fab0b
Aaron-Kao <aaronkao88@gmail.com>
2025-03-31 16:40:08 +0800

    add: db connect .env file

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+module.exports = { client };
--- server.js
+++ server.js
@@ -1,20 +1,42 @@
+// 環境變入引入
+require("dotenv").config();
 const express = require("express");
 const app = express();
 // 使用中介軟體
 const morgan = require("morgan");
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 // api引入
 const apiReview = require("./routes/Review.js");
 // db引入
 const db = require("./config/db.js");
-// 環境變入引入
-require("dotenv").config();
 //morgan
 morgan.format("Aaron", "[Aaron] :method :url :status"),
   morgan.token("from", function (req, res) {
     return req.query.from || "-";
   });
+
+// 與資料庫進行連線
+async function connDB() {
+  try {
+    await db.client.connect();
+    console.log("資料庫已連結");
+  } catch (error) {
+    console.log("資料庫連結失敗:", error);
+  }
+}
+connDB();
+
+// 關閉資料庫連線
+async function closeDB() {
+  try {
+    await client.close();
+    console.log("資料庫連線已關閉");
+  } catch (error) {
+    console.error("無法關閉資料庫連線:", error);
+  }
+}
+closeDB();
 
 // 中介軟體執行
 app.use(morgan("Aaron"));
