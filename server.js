b868e4603fe16b8892fba8864224bc1607b43dc5
Aaron-Kao <aaronkao88@gmail.com>
2025-03-31 15:06:16 +0800

    add nodemon, morgan

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
+// 使用中介軟體
+const morgan = require("morgan");
+
 const PORT = 3000;
+
+//morgan
+morgan.format("Aaron", "[Aaron] :method :url :status"),
+  morgan.token("from", function (req, res) {
+    return req.query.from || "-";
+  });
 
 app.listen(PORT, () => {
   console.log(`伺服器啟動在 http://localhost:${PORT}`);
