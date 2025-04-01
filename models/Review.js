d1935a079059a42dfa32edd4ba68be24a5848f9b
Aaron-Kao <aaronkao88@gmail.com>
2025-04-01 14:56:28 +0800

    add: review model rating

--- models/Review.js
+++ models/Review.js
@@ -7,6 +7,7 @@ const reviewSchema = new mongoose.Schema({
     required: true,
   },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  rating: { type: Number, min: 1, max: 5, required: true },
   comment: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
